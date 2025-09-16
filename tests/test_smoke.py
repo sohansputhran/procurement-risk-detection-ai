1ae2393ea2fcc8f0c7f0c758c55e59b508b5e584
Sohan Puthran <puthran.sohan@gmail.com>
2025-09-16 12:36:34 -0500

    Test Case fail fix and file restructured

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,4 +1,9 @@
-from procurement_risk_detection_ai.app.api.main import demo_score
+from src.app.api.main import demo_score
+
+import sys
+
+# adding Folder_2 to the system path
+sys.path.insert(0, "src")
 
 
 def test_demo_score_monotonic():
